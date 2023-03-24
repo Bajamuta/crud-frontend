318c079334995fae0593280f3d57efe7007c50c9
jprajzendanc <j.prajzendanc@gmail.com>
2023-03-24 09:38:13 +0100

    working on deploy

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,7 @@ const root = ReactDOM.createRoot(
 
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/*",
         element: <App/>,
         children: [
             {
